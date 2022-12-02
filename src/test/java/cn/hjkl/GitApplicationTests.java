f75a525eb57ad2e5cd9a8115dbd8ec8b37037f3a
yjj <13665792571@163.com>
2022-12-02 14:25:02 +0800

    第一次上传 v1.0

---
+++ src/test/java/cn/hjkl/GitApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hjkl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
