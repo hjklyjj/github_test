56938e3766ad2997a57b5e53484d44ed1bca428a
yjj <13665792571@163.com>
2022-12-02 14:31:20 +0800

    第2次上传 v1.1

--- src/main/java/cn/hjkl/GitApplication.java
+++ src/main/java/cn/hjkl/GitApplication.java
@@ -3,6 +3,9 @@ package cn.hjkl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author hjkl
+ */
 @SpringBootApplication
 public class GitApplication {
 
